ff69c1ef103767d7c9f1d66564dc7c989b237f1b
vladstojna <vlad.leic@gmail.com>
2022-05-14 16:42:19 +0100

    update zpic output and pp script

--- zpic/postprocess.sh
+++ zpic/postprocess.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
+function echoerr
+{
+    printf "%s\n" "$*" >&2
+}
+
+if [[ -z "$EP_PREFIX" ]]; then
+    echoerr "Environment variable EP_PREFIX not set"
+    exit 1
+fi
+
 mkdir -p "$(dirname $0)/../post-processed"
 
 scripts="$EP_PREFIX/scripts"
 
-for f in $(ls *.json); do
-    base="${f%.*}"
+for f in $(ls $(dirname $0)/*.json); do
+    base=$(basename $f)
+    no_ext="${base%.*}"
     for op in sum avg; do
         cat "$f" | \
             $scripts/remove_empty.py 2> /dev/null | \
             $scripts/compact_total_energy.py 2> /dev/null | \
             $scripts/compact_reduce_executions.py --op "$op" \
-            > "$(dirname $0)/../post-processed/${base}.${op}.json"
+            > "$(dirname $0)/../post-processed/${no_ext}.${op}.json"
     done
 done
