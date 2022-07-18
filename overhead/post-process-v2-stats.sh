448b1472740c9daabd808383536a5ada7ae212fe
vladstojna <vlad.leic@gmail.com>
2022-07-18 01:30:06 +0100

    change stats units in post process script

--- overhead/post-process-v2-stats.sh
+++ overhead/post-process-v2-stats.sh
@@ -10,11 +10,17 @@ function print_stats
         $results_scripts/file_stats_of_lines.py |
         grep 'mean\|stddev' |
         awk '{print $2}' |
+        xargs |
+        python3 -c "import sys; \
+            values = next(iter(sys.stdin)).split(' '); \
+            print('{}\n{}\n'.format(float(values[0]) $3, float(values[1]) $3))" |
         paste -d ',' -s - |
-        paste -d ',' <(echo "$1") -
+        paste -d ',' <(echo "$1,$2") -
 }
 
 results_scripts="$(dirname $0)/../scripts"
 
-echo 'column,mean,stddev'
-tee >(print_stats sample_time_delta) >(print_stats overhead) > /dev/null
+echo 'column,unit,mean,stddev'
+tee >(print_stats sample_time_delta 'ms' '/ 1000000') \
+    >(print_stats overhead '%' '* 100') > \
+    /dev/null
